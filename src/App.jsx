93a30c3af54fa4a64546ba92adf2f4db9a02c8f1
Himanshu Sharma <sweetiehimanshu@gmail.com>
2022-07-05 06:58:34 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Header from './Components/Header';
--- src/Data/config.js
+++ src/Data/config.js
@@ -1,1 +1,1 @@
-export const base_url = 'https://dharitri.herokuapp.com/products';
+export const base_url = 'https://dharitri.herokuapp.com';
